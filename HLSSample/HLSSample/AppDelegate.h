2b463bfdc5ae8f30bd5f965ffa4295e7027d13b7
wmzhong <zhongwm.net@gmail.com>
2015-10-11 12:19:56 +0800

    Make a HLS sample

---
+++ HLSSample/HLSSample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HLSSample
+//
+//  Created by Wunmin on 15/10/11.
+//  Copyright (c) 2015年 Wunmin. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ HLSSample/HLSSample/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  HLSSample
+//
+//  Created by Wunmin on 15/10/11.
+//  Copyright (c) 2015年 Wunmin. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
